f38775b66fbc0eaa169220952a19e91c952c68af
Devin Herath <devdhera@gmail.com>
2019-01-30 12:28:32 +0530

    Added docstring for models.py

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,7 +1,9 @@
+"""This used to specify the model for the project"""
 from django.db import models
 
 
 class Lead(models.Model):
+    """Configure the Lead Model"""
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
