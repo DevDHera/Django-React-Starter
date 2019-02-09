ac41c1be105a4a25923e05fc91baa99307fe69f2
Devin Herath <devdhera@gmail.com>
2019-02-09 18:08:41 +0530

    Integrated Alert layout in App.js

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -6,12 +6,13 @@ import AlertTemplate from 'react-alert-template-basic'
 
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
+import Alerts from './layout/Alerts';
 
 import { Provider } from 'react-redux';
 import store from '../store';
 
 // Alert Options
-cons alertOptions = {
+const alertOptions = {
     timeout: 3000,
     position: 'top center'
 }
@@ -23,6 +24,7 @@ class App extends Component {
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
                     <Fragment>
                         <Header />
+                        <Alerts />
                         <div className="container">
                             <Dashboard />
                         </div>
