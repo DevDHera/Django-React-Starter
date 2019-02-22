cc3460ba70f462f83d70ea5b3117fc0b1a7a0062
Devin Herath <devdhera@gmail.com>
2019-02-22 19:22:12 +0530

    Added Auth for the Backend

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -1,6 +1,5 @@
 """This used to specify the api for the project"""
 from rest_framework import viewsets, permissions
-from leads.models import Lead
 from .serializers import LeadSerializer
 
 # Lead Viewset
@@ -8,8 +7,13 @@ from .serializers import LeadSerializer
 
 class LeadViewSet(viewsets.ModelViewSet):
     """Configure the Lead Viewset"""
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
